d2b44cf7db0e3d28f9637868bee56edccf15025c
Maria Eduarda Matos <155884252+MEduardaMatos10@users.noreply.github.com>
2024-12-23 22:22:05 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function natal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'flex'; // Exibe o modal
+}
+
+// Função para fechar o modal
+function fecharModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none'; 
+}// Oculta o modal
